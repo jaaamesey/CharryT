f34440b71ba48ada17fac2e28ad144daac2bf507
Jamie <jkarlsson@mcfakefakestudios.com>
2019-04-13 17:15:35 +1000

    Added notification drawer (WIP)

--- app/src/main/java/com/group5/charryt/MainActivity.java
+++ app/src/main/java/com/group5/charryt/MainActivity.java
@@ -1,14 +1,13 @@
 package com.group5.charryt;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AlertDialog;
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +16,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Dashboard");
+
+        NavigationView navDrawer = findViewById(R.id.nav_view);
+        navDrawer.addView(new Button(getBaseContext()));
         setSupportActionBar(toolbar);
+
+        ActionBar actionbar = getSupportActionBar();
+        assert actionbar != null;
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
     }
 
     @Override
@@ -34,9 +43,15 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        if (id == R.id.action_settings) {
-            return true;
+        // Break is not needed for this switch case because return is used.
+        switch (id) {
+            case R.id.action_settings:
+                return true;
+
+            case R.id.drawer_layout:
+                return true;
         }
+
 
         return super.onOptionsItemSelected(item);
     }
---
+++ app/src/main/java/com/group5/charryt/NavigationDrawer.java
@@ -0,0 +1,5 @@
+package com.group5.charryt;
+
+public class NavigationDrawer {
+
+}
