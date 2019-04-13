0c7af7d0e55e521f5095d444bf93e05f3a96dcd1
Jamie <jkarlsson@mcfakefakestudios.com>
2019-04-13 17:38:49 +1000

    Nav drawer button now correctly opens the nav drawer

--- app/src/main/java/com/group5/charryt/MainActivity.java
+++ app/src/main/java/com/group5/charryt/MainActivity.java
@@ -2,24 +2,35 @@ package com.group5.charryt;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    // UI components
+    private NavigationView navDrawer;
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        // Instantiate references to UI components
+        navDrawer = findViewById(R.id.nav_view);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toolbar = findViewById(R.id.toolbar);
+
+
         toolbar.setTitle("Dashboard");
 
-        NavigationView navDrawer = findViewById(R.id.nav_view);
-        navDrawer.addView(new Button(getBaseContext()));
+        //navDrawer.addView(new Button(getBaseContext()));
         setSupportActionBar(toolbar);
 
         ActionBar actionbar = getSupportActionBar();
@@ -48,7 +59,8 @@ public class MainActivity extends AppCompatActivity {
             case R.id.action_settings:
                 return true;
 
-            case R.id.drawer_layout:
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
                 return true;
         }
 
